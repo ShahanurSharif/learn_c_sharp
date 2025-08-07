1055460551d6f33ac5ce907e3fd70313cc6b8d7a
sharif <shahanur700@gmail.com>
2025-08-08 00:04:41 +0800

    first

---
+++ Person.cs
@@ -0,0 +1,16 @@
+class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+    
+    public void SayHello()
+    {
+        Console.WriteLine($"Hello, my name is {Name} and I am {Age} years old.");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+class Program
+{
+
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter the first file name: ");
+        object fileName1 = Console.ReadLine();
+        
+        Console.Write("Enter the second file name: ");
+        object fileName2 = Console.ReadLine();
+
+        using var doc1 = WordprocessingDocument.Open(fileName1, false);
+        var body1 = doc1.MainDocumentPart.Document.Body;
+        
+    }
+    
+}
